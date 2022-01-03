32b2a9242bcb426ebf7146b92be8d1f594ca9ac4
jorritsandbrink <47451109+jorritsandbrink@users.noreply.github.com>
2022-01-03 15:15:26 +0100

    Update Install-DataGateway.ps1

--- Install-DataGateway.ps1
+++ Install-DataGateway.ps1
@@ -3,9 +3,56 @@ Param(
   [string]
   $ApplicationId,
   [Parameter(Mandatory = $true)]
-  [SecureString]
-  $ClientSecret
+  [string]
+  $TenantId,
+  [Parameter(Mandatory = $true)]
+  [string]
+  $ClientSecret,
+  [Parameter(Mandatory = $true)]
+  [string]
+  $RecoveryKey,
+  [Parameter(Mandatory = $true)]
+  [string]
+  $GatewayName,
+  [Parameter()]
+  [Guid]
+  $AdminObjectId
 )
 
-Write-Output $ApplicationId
-Write-Output $ClientSecret
+# Create SecureString objects
+$ClientSecretSecure = ConvertTo-SecureString $ClientSecret -AsPlainText -Force
+$RecoveryKeySecure = ConvertTo-SecureString $RecoveryKey -AsPlainText -Force
+
+# Install DataGateway module
+Write-Host "Install DataGateway powershell module"
+Install-Module -Name DataGateway -Force
+
+# Download and run gateway installer
+Install-DataGateway -AcceptConditions
+
+# Connect Service Principal
+Write-Host "Connect Service Principal"
+Connect-DataGatewayServiceAccount -ApplicationId $ApplicationId -ClientSecret $ClientSecretSecure -Tenant $TenantId | Out-Null
+
+# Thrown an error if not logged in
+Get-DataGatewayAccessToken | Out-Null
+
+# Create a gateway cluster
+Write-Host "Create gateway cluster"
+$addDataGatewayClusterArguments = @{
+  RecoveryKey = $RecoveryKeySecure;
+  GatewayName = $GatewayName;
+}
+$newGatewayClusterId = (Add-DataGatewayCluster @addDataGatewayClusterArguments).GatewayObjectId
+
+# Add gateway cluster admin
+Write-Host "Add gateway cluster admin"
+if ($null -ne $AdminObjectId) {
+  $addDataGatewayClusterUserArguments = @{
+    GatewayClusterId       = $newGatewayClusterId;
+    PrincipalObjectId      = $AdminObjectId;
+    Role                   = "Admin";
+    AllowedDataSourceTypes = $null;
+  }
+  Add-DataGatewayClusterUser @addDataGatewayClusterUserArguments
+}
